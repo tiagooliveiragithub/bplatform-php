02acb627c22e8c93ae1e6ac4c332405d55a9e580
Tiago <contacto.tiagooliveira@gmail.com>
2024-10-10 23:43:27 +0300

    Admin dashboard and nightmode

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -2,14 +2,14 @@
   <div class="section-content-wide">
     <nav class="menu">
       <h1 class="menu-item">
-        <a href="#">Blog Platform</a>
+        <a href="<?= WEBSITE_URL ?>"><i class="fa-brands fa-blogger-b"></i> Platform</a>
       </h1>
       <ul class="menu-items">
         <li class="menu-item" id="theme-toggle">
           <a style="cursor: pointer;"><i id="theme-icon" class="fa-solid fa-moon"></i></a>
         </li>
         <li class="menu-item">
-          <a href="#" class="menu-link"><i class="fa-solid fa-user"></i></a>
+          <a href="<?= WEBSITE_URL ?>dashboard.php" class="menu-link"><i class="fa-solid fa-user"></i></a>
         </li>
       </ul>
     </nav>
