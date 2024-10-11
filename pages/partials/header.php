10a42408cdaa856cffeb6aa11e508ff0a21eadc3
Tiago <contacto.tiagooliveira@gmail.com>
2024-10-12 01:59:54 +0300

    theme fix and nav bar

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -5,11 +5,19 @@
         <a href="<?= WEBSITE_URL ?>"><i class="fa-brands fa-blogger-b"></i> Platform</a>
       </h1>
       <ul class="menu-items">
+        <?php if (isset($_SESSION['user-id'])): ?>
+          <li class="menu-item">
+            <a href="<?= WEBSITE_URL ?>dashboard.php" class="menu-link"><i class="fa-solid fa-user"></i></a>
+          </li>
+          <li class="menu-item">
+            <a href="<?= WEBSITE_URL ?>signout.php" class="menu-link"><i class="fa-solid fa-sign-out"></i></a>
+          <?php else: ?>
+          <li class="menu-item">
+            <a href="<?= WEBSITE_URL ?>signin.php" class="menu-link"><i class="fa-solid fa-user"></i></a>
+          </li>
+        <?php endif ?>
         <li class="menu-item" id="theme-toggle">
           <a style="cursor: pointer;"><i id="theme-icon" class="fa-solid fa-moon"></i></a>
-        </li>
-        <li class="menu-item">
-          <a href="<?= WEBSITE_URL ?>dashboard.php" class="menu-link"><i class="fa-solid fa-user"></i></a>
         </li>
       </ul>
     </nav>
