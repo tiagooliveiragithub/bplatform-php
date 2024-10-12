769f5c36d2583f55c4918ba0b1034e8f862ecc60
GOMES31 <dugomes02@gmail.com>
2024-10-12 18:19:46 +0300

    commit

--- delete-user.php
+++ delete-user.php
@@ -10,28 +10,28 @@ if (isset($_GET['id'])) {
     $result = mysqli_query($connection, $query);
     $user = mysqli_fetch_assoc($result);
 
-    if (mysqli_num_rows($result) == 1) {
-        $avatar_name = $user['avatar'];
-        $avatar_path = '../images/' . $avatar_name;
+    // if (mysqli_num_rows($result) == 1) {
+    //     $avatar_name = $user['avatar'];
+    //     $avatar_path = 'images/' . $avatar_name;
 
-        if ($avatar_path) {
-            unlink($avatar_path);
-        }
-    }
+    //     if ($avatar_path) {
+    //         unlink($avatar_path);
+    //     }
+    // }
 
     // later we have to get all the posts and thumbnails and delete them too 
-    $thumbnails_query = "SELECT thumbnail FROM posts WHERE author_id=$id";
-    $thumbnails_result = mysqli_query($connection, $thumbnails_query);
-    if (mysqli_num_rows($thumbnails_result) > 0) {
-        while ($thumbnail = mysqli_fetch_assoc($thumbnail_result)) {
-            $thumbnail_path = 'images/' . $thumbnail['thumbnail'];
+    // $thumbnails_query = "SELECT thumbnail FROM posts WHERE author_id=$id";
+    // $thumbnails_result = mysqli_query($connection, $thumbnails_query);
+    // if (mysqli_num_rows($thumbnails_result) > 0) {
+    //     while ($thumbnail = mysqli_fetch_assoc($thumbnail_result)) {
+    //         $thumbnail_path = 'images/' . $thumbnail['thumbnail'];
 
-            // delete thumbnail from images folder is exist 
-            if ($thumbnail_path) {
-                unlink($thumbnail_path);
-            }
-        }
-    }
+    //         // delete thumbnail from images folder is exist 
+    //         if ($thumbnail_path) {
+    //             unlink($thumbnail_path);
+    //         }
+    //     }
+    // }
 
 
     // delete user from db 
--- edit-user.php
+++ edit-user.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
 <?php
-include('pages/partials/header.php');
+include('pages/partials/head.php');
 
 if (isset($_GET['id'])) {
     $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
@@ -11,40 +15,49 @@ if (isset($_GET['id'])) {
 }
 ?>
 
-<div class="website-wrapper">
-    <main class="main-content section-wrapper">
-        <div class="section-content-wide">
-            <div class="section-gap">
-                <div class="post-section">
-                    <div class="form-container">
-                        <h2>Edit User</h2>
-                        <?php if (isset($_SESSION['edit-user'])): ?>
-                            <div class="alert-msg error">
-                                <p>
-                                    <?= $_SESSION['edit-user'];
-                                    unset($_SESSION['edit-user']);
-                                    ?>
-                                </p>
-                            </div>
-                        <?php endif ?>
-                        <form action="<?= WEBSITE_URL ?>edit-user-logic.php" method="POST">
-                            <input type="hidden" value="<?= $user['id'] ?>" name="id">
-                            <input type="text" value="<?= $user['firstname'] ?>" name="firstname"
-                                placeholder="First Name">
-                            <input type="text" value="<?= $user['lastname'] ?>" name="lastname" placeholder="Last Name">
-                            <select name="userrole">
-                                <option value="0">Author</option>
-                                <option value="1">Admin</option>
-                            </select>
-                            <button type="submit" name="submit" class="btn">Update User</button>
-                        </form>
+<body>
+    <?php
+    include('pages/partials/header.php');
+    ?>
+
+    <div class="website-wrapper">
+        <main class="main-content section-wrapper">
+            <div class="section-content-wide">
+                <div class="section-gap">
+                    <div class="post-section">
+                        <div class="form-container">
+                            <h2>Edit User</h2>
+                            <?php if (isset($_SESSION['edit-user'])): ?>
+                                <div class="alert-msg error">
+                                    <p>
+                                        <?= $_SESSION['edit-user'];
+                                        unset($_SESSION['edit-user']);
+                                        ?>
+                                    </p>
+                                </div>
+                            <?php endif ?>
+                            <form action="<?= WEBSITE_URL ?>edit-user-logic.php" method="POST">
+                                <input type="hidden" value="<?= $user['id'] ?>" name="id">
+                                <input type="text" value="<?= $user['firstname'] ?>" name="firstname"
+                                    placeholder="First Name">
+                                <input type="text" value="<?= $user['lastname'] ?>" name="lastname"
+                                    placeholder="Last Name">
+                                <select name="userrole">
+                                    <option value="0">Author</option>
+                                    <option value="1">Admin</option>
+                                </select>
+                                <button type="submit" name="submit" class="btn">Update User</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
-</div>
+        </main>
+    </div>
+    <?php
+    include('pages/partials/footer.php');
+    ?>
 
-<?php
-include('../partials/footer.php');
-?>
+</body>
+
+</html>
